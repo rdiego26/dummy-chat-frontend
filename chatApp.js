61be33cff57340e023b32d14f9687039bfaf5dab
rdiego26 <rdiego26@gmail.com>
2018-02-01 17:42:26 -0200

    DOING logic

--- chatApp.js
+++ chatApp.js
@@ -2,6 +2,12 @@ function appendHistory(text) {
     let historyContainer = document.getElementById('history');
     let myNewElement = document.createElement('div');
     myNewElement.classList.add('column');
-    let myNewTextNode = document.createTextNode(text);
+    myNewElement.innerHTML = text;
     historyContainer.appendChild(myNewElement);
+}
+
+function sendUserInput() {
+    const userInput = document.getElementById('userInput').value;
+    //TODO fazer validação antes de enviar
+    appendHistory(userInput);
 }
